ba94d3f725b0f42d9e2033a486fc623740b599b2
Justin Mills <3134864+justinmills@users.noreply.github.com>
2023-05-10 18:11:40 -0400

    Replace pipenv with poetry (#8)
    Still working out how to cut/publish a release, but it's pretty good so far.
    
    Merging to test that I can run the bump release workflow on main.

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,9 +1,10 @@
-import devdeps
+import dev_dependencies as devdeps
 
 
 def test_version_is_set() -> None:
-    print(devdeps.VERSION)
+    print(f"Package version is {devdeps.VERSION} / {devdeps.__version__}")
     assert devdeps.VERSION is not None
+    assert devdeps.__version__ is not None
 
 
 # We can't really test the unset case since it's all global state. Once the
