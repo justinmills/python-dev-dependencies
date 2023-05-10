9a2e6679ca1bc955d2c7cf688e42cd0913837fb9
Justin Mills <vortexjj@gmail.com>
2023-05-10 09:46:24 -0400

    Replace pipenv with poetry
    Not entirely sold on it yet, but it does have some niceties. Will see
    about updating the build/release process next.

--- devdeps/__init__.py
+++
@@ -1,8 +0,0 @@
-from importlib.metadata import PackageNotFoundError, version
-
-VERSION: str | None = None
-try:
-    VERSION = version("dev-dependencies")
-except PackageNotFoundError:  # pragma: no cover
-    # package is not installed
-    pass
